ed98cb9e4a62f69a5d9a8dc0c81f90f13f429357
lsumin223 <sumin99223@gmail.com>
2024-01-26 12:30:59 +0900

    AR 고개 회전 판정, 전투 매니저 생성

---
+++ Assets/Scripts/LSM/BattleManager.cs
@@ -0,0 +1,150 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class BattleManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public Transform enemyBattleTransform;
+    Unit enemyUnit;
+
+    //public BattleHUD playerHUD;
+    //public BattleHUD enemyHUD;
+
+    public Text dialogueText;
+    public BattleState state;
+
+    public Button attackButton;
+    public Button healButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = BattleState.START;
+        StartCoroutine(SetupBattle());
+
+        attackButton.interactable = false;
+        healButton.interactable = false;
+    }
+
+    // Update is called once per frame
+    IEnumerator SetupBattle()
+    {
+        GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleTransform);
+        enemyUnit = enemyGO.GetComponent<Unit>();
+
+        dialogueText.text = enemyUnit.unitName;
+
+        //playerHUD.SetHUD(GameManager.instance.playerUnit);
+        // enemyHUD.SetHUD(enemyUnit);
+
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
+    }
+
+    IEnumerator PlayerAttack()
+    {
+        bool isDead = enemyUnit.TakeDamage(GameManager.instance.playerUnit.damage);
+
+        //enemyHUD.SetHP(enemyUnit.currentHP);
+        dialogueText.text = "The Attack is sucessful!";
+
+        yield return new WaitForSeconds(2f);
+
+        if (isDead)
+        {
+            state = BattleState.WON;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.ENEMYTURN;
+            StartCoroutine(EnemyTurn());
+        }
+        //change state based on what happened
+    }
+
+    IEnumerator EnemyTurn()
+    {
+        dialogueText.text = enemyUnit.unitName + "attack!";
+
+        yield return new WaitForSeconds(1f);
+
+        bool isDead = GameManager.instance.playerUnit.TakeDamage(enemyUnit.damage);
+
+        //playerHUD.SetHP(GameManager.instance.playerUnit.currentHP);
+
+        yield return new WaitForSeconds(1f);
+
+        if (isDead)
+        {
+            state = BattleState.LOST;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.WON;
+            EndBattle();
+        }
+    }
+
+    void EndBattle()
+    {
+        if (state == BattleState.WON)
+        {
+            dialogueText.text = "You won!";
+        }
+        else if (state == BattleState.LOST)
+        {
+            dialogueText.text = "You lose";
+        }
+    }
+
+    void PlayerTurn()
+    {
+        attackButton.interactable = true;
+        healButton.interactable = true;
+
+        dialogueText.text = "Choose an action:";
+    }
+
+    IEnumerator PlayerHeal()
+    {
+        GameManager.instance.playerUnit.Heal(5);
+
+        //playerHUD.SetHP(GameManager.instance.playerUnit.currentHP);
+        dialogueText.text = "Heal!";
+
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.ENEMYTURN;
+        StartCoroutine(EnemyTurn());
+
+    }
+
+    public void OnAttackButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
+
+        attackButton.interactable = false;
+        healButton.interactable = false;
+
+        StartCoroutine(PlayerAttack());
+    }
+
+    public void OnHealButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
+
+        attackButton.interactable = false;
+        healButton.interactable = false;
+
+        StartCoroutine(PlayerHeal());
+    }
+}
