209e309e542544cf25112798e3f20c534d854d52
koo6132 <midnight3338@gmail.com>
2024-02-01 17:15:30 +0900

    koo6132
    사용자 위치 기반 랜덤스폰

--- Assets/JSH/MapTest.cs
+++ Assets/JSH/MapTest.cs
@@ -11,6 +11,8 @@ public class MapTest: MonoBehaviour
     public GameObject C;
     public GameObject D;
 
+   
+   
     void Start()
     {
         GetObjectCoordinates();
@@ -18,19 +20,18 @@ public class MapTest: MonoBehaviour
 
     void GetObjectCoordinates()
     {
-        if (_map != null)
+        if (_map != null && A != null)
         {
-            // 현재 오브젝트의 위치를 Unity 좌표에서 지도상의 좌표로 변환
-            Vector2d AobjectCoordinates = _map.WorldToGeoPosition(A.transform.position);
-
-            // 변환된 좌표 출력
-            Debug.Log("Object Coordinates (Latitude, Longitude): " + AobjectCoordinates.x + ", " + AobjectCoordinates.y);
-            double AA = Mathf.Clamp((float)AobjectCoordinates.y,-180f,180f);
-            Debug.Log(AA);
-        }
-        else
-        {
-            Debug.LogError("Map reference is not set!");
+            // 오브젝트의 유니티 좌표를 지도상의 좌표로 변환
+            Vector2d geoCoordinates = _map.WorldToGeoPosition(A.transform.position);
+            if (!double.IsInfinity(geoCoordinates.x) && !double.IsInfinity(geoCoordinates.y))
+            {
+                Debug.Log("Object Coordinates (Latitude, Longitude): " + geoCoordinates.x + ", " + geoCoordinates.y);
+            }
+            else
+            {
+                Debug.LogError("Invalid or out-of-bounds coordinates!");
+            }
         }
     }
 }
