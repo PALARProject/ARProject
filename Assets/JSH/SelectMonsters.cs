3776c299ef70b08816ce4f0b6053d6f99669b020
koo6132 <midnight3338@gmail.com>
2024-02-05 19:50:01 +0900

    koo6132
    레이 수정조금남음

---
+++ Assets/JSH/SelectMonsters.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectMonsters : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            
+            if (touch.phase == TouchPhase.Began)
+            {
+                Vector3 screen = new Vector3(touch.position.x, touch.position.y, 0);
+                Vector3 touchPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x,touch.position.y,120));
+                Debug.Log("P"+touch.position);
+                Debug.Log("M"+Input.mousePosition);
+                Debug.Log(touchPos);
+                //Vector3 touchPos = Camera.main.transform.position + new Vector3(touch.position.x,0,touch.position.y);
+
+                Vector3 rayvec = touchPos-Camera.main.transform.position;
+
+                RaycastHit2D hit = Physics2D.Raycast(Camera.main.transform.position, rayvec);
+                Debug.Log(hit.collider);
+                Debug.DrawRay(Camera.main.transform.position, rayvec , Color.red, 1f);
+                if (hit.collider != null)
+                {
+                    // 터치한 오브젝트를 처리하는 함수 호출
+                   SelectMonster(hit.collider.gameObject);
+                }
+            }
+        }
+    }
+    public void SelectMonster(GameObject selectobject)
+    {
+        if (selectobject.tag == "Enemy")
+        {
+            SceneManager.LoadScene("BattleScene");
+        }
+        else
+            Debug.Log(selectobject.name);
+    }
+}
