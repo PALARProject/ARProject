80da0ce3aebbcf41973c870924257b3353c137b9
koo6132 <midnight3338@gmail.com>
2024-02-06 01:10:47 +0900

    koo6132
    GPS 터치 씬이동 테스트 예정

--- Assets/JSH/SelectMonsters.cs
+++ Assets/JSH/SelectMonsters.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class SelectMonsters : MonoBehaviour
 {
+    GameObject obj;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +13,7 @@ public class SelectMonsters : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+  void Update()
     {
         if (Input.touchCount > 0)
         {
@@ -32,21 +33,38 @@ public class SelectMonsters : MonoBehaviour
                 RaycastHit2D hit = Physics2D.Raycast(Camera.main.transform.position, rayvec);
                 Debug.Log(hit.collider);
                 Debug.DrawRay(Camera.main.transform.position, rayvec , Color.red, 1f);
-                if (hit.collider != null)
-                {
-                    // 터치한 오브젝트를 처리하는 함수 호출
-                   SelectMonster(hit.collider.gameObject);
-                }
+
             }
         }
     }
-    public void SelectMonster(GameObject selectobject)
+    
+    public void OnTriggerEnter(Collider other)
     {
-        if (selectobject.tag == "Enemy")
+
+        if (other.tag == "Enemy")
         {
-            SceneManager.LoadScene("BattleScene");
-        }
-        else
-            Debug.Log(selectobject.name);
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+
+                if (touch.phase == TouchPhase.Began)
+                {
+                    Vector3 screen = new Vector3(touch.position.x, touch.position.y, 0);
+                    Vector3 touchPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 120));
+                    Debug.Log("P" + touch.position);
+                    Debug.Log("M" + Input.mousePosition);
+                    Debug.Log(touchPos);
+                    //Vector3 touchPos = Camera.main.transform.position + new Vector3(touch.position.x,0,touch.position.y);
+
+                    Vector3 rayvec = touchPos - Camera.main.transform.position;
+
+                    RaycastHit2D hit = Physics2D.Raycast(Camera.main.transform.position, rayvec);
+                    Debug.Log(hit.collider);
+                    Debug.DrawRay(Camera.main.transform.position, rayvec, Color.red, 1f);
+                    
+                }
+            }
+          //  SceneManager.LoadScene("BattleScene")
+;        }
     }
 }
