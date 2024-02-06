9e28e2b7b2fa8631dc8b3dbb89a89536faa63759
koo6132 <midnight3338@gmail.com>
2024-02-06 17:35:09 +0900

    koo6132
    1차완

--- Assets/JSH/SelectMonsters.cs
+++ Assets/JSH/SelectMonsters.cs
@@ -2,69 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SelectMonsters : MonoBehaviour
 {
     GameObject obj;
+    public GameObject Montext;
+    private RaycastHit Hit2;
+    private int i;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Montext.SetActive(false);
     }
 
     // Update is called once per frame
-  void Update()
+    void Update()
     {
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            
+
             if (touch.phase == TouchPhase.Began)
             {
                 Vector3 screen = new Vector3(touch.position.x, touch.position.y, 0);
-                Vector3 touchPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x,touch.position.y,120));
-                Debug.Log("P"+touch.position);
-                Debug.Log("M"+Input.mousePosition);
+                Vector3 touchPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 120));
+                Debug.Log("P" + touch.position);
+                Debug.Log("M" + Input.mousePosition);
                 Debug.Log(touchPos);
                 //Vector3 touchPos = Camera.main.transform.position + new Vector3(touch.position.x,0,touch.position.y);
 
-                Vector3 rayvec = touchPos-Camera.main.transform.position;
+                Vector3 rayvec = touchPos - Camera.main.transform.position;
 
-                RaycastHit2D hit = Physics2D.Raycast(Camera.main.transform.position, rayvec);
+                RaycastHit hit;
+                Physics.Raycast(Camera.main.transform.position, rayvec,out hit);
+                Hit2 = hit;
                 Debug.Log(hit.collider);
-                Debug.DrawRay(Camera.main.transform.position, rayvec , Color.red, 1f);
-
+                Debug.DrawRay(Camera.main.transform.position, rayvec, Color.red, 1f);
+            }
+            if (Hit2.collider != null && Hit2.collider.tag == "Enemy"&& i == 1 )
+            {
+                SceneManager.LoadScene("BattleScene");
             }
         }
     }
-    
-    public void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider collision)
     {
-
-        if (other.tag == "Enemy")
+        if (collision.tag == "Enemy")
         {
-            if (Input.touchCount > 0)
-            {
-                Touch touch = Input.GetTouch(0);
-
-                if (touch.phase == TouchPhase.Began)
-                {
-                    Vector3 screen = new Vector3(touch.position.x, touch.position.y, 0);
-                    Vector3 touchPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 120));
-                    Debug.Log("P" + touch.position);
-                    Debug.Log("M" + Input.mousePosition);
-                    Debug.Log(touchPos);
-                    //Vector3 touchPos = Camera.main.transform.position + new Vector3(touch.position.x,0,touch.position.y);
-
-                    Vector3 rayvec = touchPos - Camera.main.transform.position;
-
-                    RaycastHit2D hit = Physics2D.Raycast(Camera.main.transform.position, rayvec);
-                    Debug.Log(hit.collider);
-                    Debug.DrawRay(Camera.main.transform.position, rayvec, Color.red, 1f);
-                    
-                }
-            }
-          //  SceneManager.LoadScene("BattleScene")
-;        }
+            i = 1;
+            Montext.SetActive(true);
+            Debug.Log(collision.transform.position);
+        }
     }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Enemy")
+        {
+            i = 0;
+            Montext.SetActive(false);
+        }
+    }
+
 }
